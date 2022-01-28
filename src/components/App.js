90fc2f5c40342be2a7b4f794b9701cb46895d6f2
david-aihe <doa9200@gmail.com>
2022-01-27 22:26:04 -0500

    Routes in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import styled from 'styled-components';
+import PrivateRoute from './PrivateRoute';
 
 import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
+import Logout from './Logout';
+import View from './View';
 
 const App = () => {
   return (
     <AppContainer>
       <BloomHeader/>
-      <Header/>
-      <RouteContainer>
-        <Route exact path="/">
-          <Login/>
-        </Route>          
-      </RouteContainer>
+      <Router>
+        <Header/>
+        <RouteContainer>
+
+          <Switch>
+            <PrivateRoute path='/logout' component={Logout} />
+            <PrivateRoute path='/view' component={View} />
+            <Route exact path="/">
+              <Login/>
+            </Route>  
+          </Switch>    
+
+        </RouteContainer>
+      </Router>
     </AppContainer>
   )
 }
