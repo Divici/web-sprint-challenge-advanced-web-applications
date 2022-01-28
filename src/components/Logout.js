5c1e4e8d6b7b2c9e40e916a839603f9a758e77fe
david-aihe <doa9200@gmail.com>
2022-01-28 00:03:16 -0500

    Logout.js finished

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import axios from 'axios';
 
-const Logout = () => {        
+const Logout = () => { 
+    const {push} = useHistory();
+
+    useEffect(()=> {
+        const token = localStorage.getItem('token');
+
+        axios.post('http://localhost:5000/api/logout', {}, {
+            headers: {
+                authorization: token
+            }
+        })
+        .then(resp=>{
+            localStorage.removeItem('token');
+            push('/');
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }, [])
+    
     return(<div></div>);
 }
 
 export default Logout;
-
-// Task List
-// 1. On mount, execute a http request to the logout endpoint.
-// 2. On a successful request, remove the token from localStorage and redirect to the login page.
